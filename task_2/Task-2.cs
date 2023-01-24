828761a344cd692fd5c1be36582ee9bdfb38e051
BelovEA <vrachea676@gmail.com>
2023-01-24 20:02:59 +0300

    add task_2, compleate!

---
+++ task_2/Task-2.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Данное приложение  выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("третьей цифры нет!");
+}
+else
+{
+    Console.WriteLine("Последняя цифра заданного числа " + num + " равна: " + (num % 10));
+}
