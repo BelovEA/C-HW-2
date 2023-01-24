8b60b1b5886368965406a2ad5d6d33497171838b
BelovEA <vrachea676@gmail.com>
2023-01-24 20:21:38 +0300

    make 3 task and pull_req

---
+++ task_3/task_3.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Данная программа, принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+Console.Write("Укажите цифру, обозначающую день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 7 || num == 6)
+{
+    Console.WriteLine("адихаем");
+}
+else
+{
+    Console.WriteLine("опять работать :(");
+}
+
+//all
