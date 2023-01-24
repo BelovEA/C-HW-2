da95e750ce8d0e77f5eedb601a68ad310be90c60
BelovEA <vrachea676@gmail.com>
2023-01-24 19:50:27 +0300

    make tast_1

---
+++ task_1/task_1.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Данная программа принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Console.Write("Укажите целое число, отличное от нуля: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int number = num / 10;
+int second_num = number % 10;
+Console.WriteLine("Вторая цифра числа " + num + "  это " + second_num);
+
